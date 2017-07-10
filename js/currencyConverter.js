8fa3512c411e91bb515496ab45f47d4a586324e3
Paul <ramshaw@hotmail.co.uk>
2017-07-10 20:24:44 +0100

    added prompts

--- js/currencyConverter.js
+++ js/currencyConverter.js
@@ -11,6 +11,9 @@ CurrencyConverter.prototype = {
   setRates: function (rates){
       this._rates = rates
       return 
-  }
-}
+  },
+  convertTo: function (currency, amount) {
+      var toRate = Number(this._rates[currency])
+      return (toRate * amount)
+  }}
 
