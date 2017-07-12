8110f34ec2c68314d154c27ee3eed9c3945ce09c
jez23 <jblackmoreuk@hotmail.co.uk>
2017-07-12 21:12:30 +0100

    fixed

--- js/form.js
+++ js/form.js
@@ -1,14 +1,20 @@
 $(document).ready(function () {
     $("#js-convert-gbp").submit(function (event) { 
         event.preventDefault()
+          var target = event.target 
+          var amount = target.amount.value
+          var currency = target.currency.value
+          var convertedAmount = currencyConverter.convertTo(currency, amount)
 
-        var target = event.target 
-        var amount = target.amount.value
-        var currency = target.currency.value
-         var convertedAmount = currencyConverter.convertTo(currency, amount)
+console.log(currency)
+        if (!amount || !currency){
+            return alert("Please enter a value!")
+          }
 
+        else {
+       
          $("#result").append(amount + " GBP in " + currency + " is " + convertedAmount.toFixed(2))
-         
+        }
     } ) 
 
 }) 
