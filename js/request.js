52402620aeec3ef3aa55f424e6bc02741464679e
jez23 <jblackmoreuk@hotmail.co.uk>
2017-07-12 20:49:18 +0100

    changed

--- js/request.js
+++ js/request.js
@@ -1,7 +1,7 @@
 var currencyConverter = new CurrencyConverter 
 
 var request = new XMLHttpRequest() 
-request.open("GET", "http://api.fixer.io/latest?base=GBP")
+request.open("GET", "https://api.fixer.io/latest?base=GBP")
 request.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
         var responseJSON = JSON.parse(this.response)
