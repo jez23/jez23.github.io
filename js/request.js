65dbe23f41f689b4c16969882c970bf50c9a4e78
Paul <ramshaw@hotmail.co.uk>
2017-07-12 20:05:54 +0100

    css styling

--- js/request.js
+++ js/request.js
@@ -10,12 +10,16 @@ request.onreadystatechange = function () {
         currencyConverter.setRates(rates)
          for (var rate in rates) {
             $("#rates").append("<li><strong>" + rate + ":</strong> " + rates[rate] + "</li>")
-$("#ratesDropdown").append('<option value="' + rate + '">' + rate + '</option>')
+            $("#ratesDropdown").append('<option value="' + rate + '">' + rate + '</option>')
          }  
         /* var amount = prompt("how much GBP are you looking to convert?")
          var currency = prompt("what currency would you like to convert to (enter 3 digit code in caps e.g. EUR)?")
          var convertedAmount = currencyConverter.convertTo(currency, amount)
          alert(amount + " GBP in " + currency + " is " + convertedAmount.toFixed(2))*/
+
+         setTimeout(function() {
+             $("#overlay").hide()
+         }, 2000);
     }    
 }
 request.send()
